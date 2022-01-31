ac0d54b19c6a8b9b52d6557a277070d5bcfd7314
Your name here <irava2@uic.edu>
2022-01-31 13:22:42 -0600

    FirstMiniProject

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+  
+  function draw() {
+    background(220);
+    
+     strokeWeight(1);
+  stroke(255,0,0);
+    fill(250,0,0);
+    
+     line(95,0,105,775);
+    stroke(240);
+  strokeWeight(20);
+    
+    circle(40,75,100)
+    
+       line(2225,0,250,725);
+    stroke(240);
+  strokeWeight(20);
+    
+    circle(160,195,100)
+    
+    
+  
+  
+  
+  }
+   
